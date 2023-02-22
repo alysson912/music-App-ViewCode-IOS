613ca39519517ea7e6e623a36ab88c65b3e358d4
Alysson Menezes <almenezes912@gmail.com>
2023-02-22 15:59:29 -0300

    entrando em conformidade com os protocolos da tableView, criando cell  customizavel  e seu respectivo init.

---
+++ soundMusic/soundMusic/src/Home/Cell/CardViewTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CardViewTableViewCell.swift
+//  soundMusic
+//
+//  Created by ALYSSON DODO on 22/02/23.
+//
+
+import UIKit
+
+class CardViewTableViewCell: UITableViewCell {
+
+    static let identifier: String = "CardViewTableViewCell"
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- soundMusic/soundMusic/src/Home/ViewController/HomeVC.swift
+++ soundMusic/soundMusic/src/Home/ViewController/HomeVC.swift
@@ -13,7 +13,9 @@ class HomeVC: UIViewController {
     
     override func loadView() {
         screen = HomeScreen()
+        screen?.configTableViewProtocols(delegate: self, dataSource: self) // assinando metodo la da screen
         view = screen
+        
     }
     
     override func viewDidLoad() {
@@ -22,7 +24,29 @@ class HomeVC: UIViewController {
        
     }
 
-
 }
 
+// entrando em conformidade com protocolos
+extension HomeVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CardData.count // model externo
 
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 500
+    }
+    
+   
+}
